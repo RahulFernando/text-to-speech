f7fa1e9fc5599d8dd7dae79579620b0ed1c1a73a
RahulFernando <rahulf@itx360.com>
2023-04-24 12:28:12 +0530

    Init

---
+++ TextToSpeechSinhala/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.CognitiveServices.Speech;
+using Microsoft.CognitiveServices.Speech.Audio;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        var config = SpeechConfig.FromSubscription("bbddf590ab2740fdbefc1a3f21f6355c", "eastus");
+        config.SpeechSynthesisLanguage = "si-LK";
+        config.SetSpeechSynthesisOutputFormat(SpeechSynthesisOutputFormat.Riff16Khz16BitMonoPcm);
+
+        using var audioConfig = AudioConfig.FromDefaultSpeakerOutput();
+        using var synthesizer = new SpeechSynthesizer(config, audioConfig);
+
+        var result = await synthesizer.SpeakTextAsync("සුබ දවසක්!");
+
+        if (result.Reason == ResultReason.SynthesizingAudioCompleted)
+        {
+            Console.WriteLine($"Speech synthesis succeeded.");
+        }
+        else if (result.Reason == ResultReason.Canceled)
+        {
+            var cancellation = SpeechSynthesisCancellationDetails.FromResult(result);
+            Console.WriteLine($"Speech synthesis was canceled: {cancellation.Reason}");
+
+            if (cancellation.Reason == CancellationReason.Error)
+            {
+                Console.WriteLine($"Error details: {cancellation.ErrorDetails}");
+            }
+        }
+    }
+
+}
